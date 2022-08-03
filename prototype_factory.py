be1a4e283a1c44e730e8e21f9237faa149c38df3
josezerozeta <josezerozeta@gmail.com>
2022-08-03 10:46:50 +0200

    prototype factory pattern

---
+++ prototype_factory.py
@@ -0,0 +1,47 @@
+import copy
+
+
+class Address:
+    def __init__(self, street_address, suite, city):
+        self.city = city
+        self.street_address = street_address
+        self.suite = suite
+
+    def __str__(self):
+        return f'{self.street_address}, Suite #{self.suite}, {self.city}'
+
+
+class Employee:
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
+
+    def __str__(self):
+        return f'{self.name} works at {self.address}'
+
+
+class EmployeeFactory:
+    main_office_employee = Employee('', Address('123 East Dr', 0, 'London'))
+    aux_office_employee = Employee('', Address('123B East Dr', 0, 'London'))
+
+    @staticmethod
+    def __new_employee(prototype, name, suite):
+        result = copy.deepcopy(prototype)
+        result.name = name
+        result.address.suite = suite
+        return result
+
+    @staticmethod
+    def new_main_office_employee(name, suite):
+        return EmployeeFactory.__new_employee(EmployeeFactory.main_office_employee, name, suite)
+
+    @staticmethod
+    def new_aux_office_employee(name, suite):
+        return EmployeeFactory.__new_employee(EmployeeFactory.aux_office_employee, name, suite)
+
+
+john = EmployeeFactory.new_main_office_employee('John', 101)
+jane = EmployeeFactory.new_aux_office_employee('Jane', 500)
+
+print(john)
+print(jane)
