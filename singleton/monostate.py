bbe54acf09748ea5c56c0cbee28a1b0ead2e7bc3
josezerozeta <josezerozeta@gmail.com>
2022-08-04 16:00:58 +0200

    singleton pattern

---
+++ singleton/monostate.py
@@ -0,0 +1,28 @@
+class Monostate:
+    _shared_state = {}
+
+    def __new__(cls, *args, **kwargs):
+        obj = super(Monostate, cls).__new__(cls, *args, **kwargs)
+        obj.__dict__ = cls._shared_state
+        return obj
+
+
+class CFO(Monostate):
+    def __init__(self):
+        self.name = ''
+        self.money_managed = 0
+
+    def __str__(self):
+        return f'{self.name} manages {self.money_managed}'
+
+
+if __name__ == '__main__':
+    cfo1 = CFO()
+    cfo1.name = 'Sheryl'
+    cfo1.money_managed = 1
+    print(cfo1)
+
+    cfo2 = CFO()
+    cfo2.name = 'Ruth'
+    cfo2.money_managed = 10
+    print(cfo1, cfo2, sep='\n')
